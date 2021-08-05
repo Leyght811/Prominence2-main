b3e3cb628814c4544763da1e7475be16ebd87a27
Leyght <savier811@gmail.com>
2021-08-06 11:05:56 +1200

    Footer Finished

--- src/components/Blocks/Footer.js
+++ src/components/Blocks/Footer.js
@@ -5,7 +5,8 @@ import "../../assets/styles/Footer.css"
 const Footer = () => {
     return(
         <footer className="footer Text-Block">
-            a
+            <div style={{lineHeight:"20px"}}>06-343 1011 or 021 2424 800</div>
+            <div style={{lineHeight:"20px"}}>©2021 by Prominence Curtains And Blinds.</div>
         </footer>
     )
 }
