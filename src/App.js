f8d0799407d1c46dc5e992a5413917d1ad5efdcd
Leyght <savier811@gmail.com>
2021-08-05 16:55:07 +1200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import "./assets/styles/Style.css";
+import "./assets/styles/Coloring.css";
+import "./assets/styles/Elements.css";
+import "./assets/styles/Containers.css";
+import "./assets/styles/Transitions.css";
+
+class App extends Component {
+  
+};
+
+export default connect()(App);
